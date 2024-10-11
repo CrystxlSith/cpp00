f4a214852a153b258d8e0a45a4bb3c51301f78bc
CrystalSith <joaquim1810@gmail.com>
2024-10-11 09:35:11 +0200

    Add Contact and PhoneBook classes; remove unnecessary comments from megaphone.cpp

---
+++ ex01/Contact.hpp
@@ -0,0 +1,22 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+class Contact
+{
+private:
+    /* data */
+public:
+    Contact(/* args */);
+    ~Contact();
+};
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
+
+
+#endif
